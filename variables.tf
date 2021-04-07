e6aa46917042c063808f3f21a784ad634a3aa78f
Kaiohenriqueps <kh.silva@hotmail.com>
2021-04-07 12:16:06 -0300

    removendo variáveis do sg e subnets

--- variables.tf
+++ variables.tf
@@ -32,35 +32,3 @@ variable sqs_policies {
     "sqs:*"
   ]
 }
-
-variable "cidr_block" {
-    type    = string
-    default = "192.168.0.0/24"
-}
-
-variable "vpc_name" {
-    type    = string
-    default = "TerraformVPC"
-}
-
-variable "ingressrules" {
-    type    = list(number)
-    default = [80, 443]
-}
-
-variable "egressrules" {
-    type    = list(number)
-    default = [80, 443, 25, 3306, 53, 8080]
-}
-
-variable "subnet_ids" {
-    type = list(string)
-    default = [
-        "subnet-b41ebcd2",
-        "subnet-fd6700f3",
-        "subnet-c134c5f0",
-        "subnet-529b3373",
-        "subnet-3b542976",
-        "subnet-f2db75ad"
-    ]
-}
