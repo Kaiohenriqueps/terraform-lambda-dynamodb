b3792958574dfb06fb5317e41ea6fa6323ebffaf
Kaiohenriqueps <kh.silva@hotmail.com>
2021-04-08 09:41:06 -0300

    mudança na tabela do dynamodb

--- aws-dynamodb.tf
+++ aws-dynamodb.tf
@@ -1,17 +1,23 @@
 resource "aws_dynamodb_table" "teste" {
-    name = "tabela_teste_terraform"
-    billing_mode = "PROVISIONED"
-    read_capacity  = 2
-    write_capacity = 2
-    hash_key = "cpf"
+  name           = "tabela_teste_terraform"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 2
+  write_capacity = 2
+  hash_key       = "user_id"
+  range_key      = "item_id"
 
-    server_side_encryption {
-        enabled = true
-        kms_key_arn = aws_kms_key.dynamodb.arn
-    }
+  server_side_encryption {
+    enabled     = true
+    kms_key_arn = aws_kms_key.dynamodb.arn
+  }
 
-    attribute {
-        name = "cpf"
-        type = "S"
-    }
+  attribute {
+    name = "user_id"
+    type = "N"
+  }
+
+  attribute {
+    name = "item_id"
+    type = "N"
+  }
 }
